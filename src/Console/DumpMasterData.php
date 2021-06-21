25283fc2dc45fe582410c4e31c2effb6ba2dcf41
Muhammad Talha <talha8018@gmail.com>
2021-06-21 11:33:37 +0500

    update

--- src/Console/DumpMasterData.php
+++ src/Console/DumpMasterData.php
@@ -95,7 +95,21 @@ class DumpMasterData extends Command
         ChartOfAccount::updateOrCreate(['id' => '62'], ['name' => 'Income - OPD', 'type' => 'Income', 'sub_account' => '36', 'level' => '5', 'nature' => 'c','reference' => 'income-opd-5']);
         ChartOfAccount::updateOrCreate(['id' => '63'], ['name' => 'Employee Share Payable', 'type' => 'Liabilities', 'sub_account' => '23', 'level' => '3', 'nature' => 'c']);
         ChartOfAccount::updateOrCreate(['id' => '64'], ['name' => 'Employee Share Payable', 'type' => 'Liabilities', 'sub_account' => '63', 'level' => '4', 'nature' => 'c','reference'=>'employee-share-payable-4']);
-        ChartOfAccount::updateOrCreate(['id' => '65'], ['name' => 'Expense Commission OPD', 'reference' => 'expense-commission-opd-5','type' => 'Expenses', 'sub_account' => '61', 'level' => '5', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '65'], ['name' => 'Expense OPD Commission', 'reference' => 'expense-commission-opd-5','type' => 'Expenses', 'sub_account' => '61', 'level' => '5', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '66'], ['name' => 'Cash at Pharmacy Tills', 'reference' => 'cash-at-pharmacy-tills-4', 'type' => 'Assets', 'sub_account' => '10', 'level' => '4', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '67'], ['name' => 'Pharmacy Till 1', 'reference' => 'pharmacy-till-1-5', 'type' => 'Assets', 'sub_account' => '66', 'level' => '5', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '68'], ['name' => 'Cost of Sales - Pharmacy', 'reference' => 'cost-of-sales-pharmacy-5','type' => 'Expenses', 'sub_account' => '61', 'level' => '5', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '69'], ['name' => 'Income - Pharmacy', 'type' => 'Income', 'sub_account' => '36', 'level' => '5', 'nature' => 'c','reference' => 'income-pharmacy-5']);
+        ChartOfAccount::updateOrCreate(['id' => '70'], ['name' => 'Expense IPD OTA Commission', 'reference' => 'expense-ipd-ota-commission-5','type' => 'Expenses', 'sub_account' => '61', 'level' => '5', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '71'], ['name' => 'Expense IPD Doctor Commission', 'reference' => 'expense-ipd-doctor-commission-5','type' => 'Expenses', 'sub_account' => '61', 'level' => '5', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '72'], ['name' => 'Expense IPD Nurse Commission', 'reference' => 'expense-ipd-nurse-commission-5','type' => 'Expenses', 'sub_account' => '61', 'level' => '5', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '73'], ['name' => 'Expense IPD Anesthesia Commission', 'reference' => 'expense-ipd-anesthesia-commission-5','type' => 'Expenses', 'sub_account' => '61', 'level' => '5', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '73'], ['name' => 'Expense IPD Medicine Commission', 'reference' => 'expense-ipd-medicine-commission-5','type' => 'Expenses', 'sub_account' => '61', 'level' => '5', 'nature' => 'd']);
+        ChartOfAccount::updateOrCreate(['id' => '74'], ['name' => 'Income - IPD', 'type' => 'Income', 'sub_account' => '36', 'level' => '5', 'nature' => 'c','reference' => 'income-ipd-5']);
+        ChartOfAccount::updateOrCreate(['id' => '75'], ['name' => 'Trade Payable', 'type' => 'Liabilities', 'sub_account' => '23', 'level' => '3', 'nature' => 'c']);
+        ChartOfAccount::updateOrCreate(['id' => '76'], ['name' => 'Trade Payable', 'type' => 'Liabilities', 'sub_account' => '75', 'level' => '4', 'nature' => 'c']);
+        ChartOfAccount::updateOrCreate(['id' => '77'], ['name' => 'Indoor Advances', 'reference'=>'indoor-advances-5', 'type' => 'Liabilities', 'sub_account' => '76', 'level' => '5', 'nature' => 'c']);
+        ChartOfAccount::updateOrCreate(['id' => '78'], ['name' => 'Cash in Hand - Muhammad Talha',  'type' => 'Assets', 'sub_account' => '12', 'level' => '5', 'nature' => 'd']);
         $this->info('Dumping Finished...');
     }
 }
