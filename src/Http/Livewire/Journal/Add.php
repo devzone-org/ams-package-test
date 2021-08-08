64c7f4b7342073bb06ae9ef487fb5844140bd769
Muhammad Talha <talha8018@gmail.com>
2021-08-08 22:51:14 +0500

    updaete

--- src/Http/Livewire/Journal/Add.php
+++ src/Http/Livewire/Journal/Add.php
@@ -302,7 +302,7 @@ class Add extends Component
             DB::beginTransaction();
 
             if (Ledger::where('voucher_no', $this->voucher_no)
-                ->where('posted_by', '!=', Auth::user()->id)->exists()) {
+                ->where('posted_by', '!=', Auth::user()->id)->where('is_approve','f')->exists()) {
                 $this->addError('voucher_no', 'Voucher # ' . $this->voucher_no . ' already in use. System have updated to new one kindly try again.');
                 $this->voucher_no = Voucher::instance()->tempVoucherOnly();
                 return false;
