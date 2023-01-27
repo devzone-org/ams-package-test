06b78a1279e7cb16b43ee18f95af91c7ee1c6f1f
Touqeer Asad <touqeerasad805@gmail.com>
2023-01-27 10:26:55 +0500

    profit and loss report datewise fixed

--- resources/views/livewire/reports/profit-loss-date-wise.blade.php
+++ resources/views/livewire/reports/profit-loss-date-wise.blade.php
@@ -7,7 +7,7 @@
             <div class="grid grid-cols-6 gap-6">
                 <div class="col-span-6 sm:col-span-2">
                     <label for="from_date" class="block text-sm font-medium text-gray-700">From Date</label>
-                    <input type="text" wire:model.lazy="from_date"  readonly  id="from_date" autocomplete="off"
+                    <input type="text" wire:model.lazy="from_date" readonly id="from_date" autocomplete="off"
                            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                 </div>
 
@@ -24,12 +24,13 @@
                             <span wire:loading wire:target="search">Searching ...</span>
                             <span wire:loading.remove wire:target="search">Search</span>
                         </button>
-                        <button type="button" wire:click="resetSearch" wire:loading.attr="disabled"
-                                class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                            Reset
-                        </button>
+{{--                        <button type="button" wire:click="resetSearch" wire:loading.attr="disabled"--}}
+{{--                                class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">--}}
+{{--                            Reset--}}
+{{--                        </button>--}}
                         @if(!empty($report))
-                            <a href="{{'date-wise/export'}}?from_date={{date('d M Y', strtotime($from_date))}}&to_date={{date('d M Y', strtotime($to_date))}}" target="_blank"
+                            <a href="{{'date-wise/export'}}?from_date={{date('d M Y', strtotime($from_date))}}&to_date={{date('d M Y', strtotime($to_date))}}"
+                               target="_blank"
                                class="ml-3 disabled:opacity-30 bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none ">
                                 Export.csv
                             </a>
@@ -82,16 +83,16 @@
                 </div>
             @endif
         </div>
-        <div class=""
+        <div class="overflow-auto" style="width: 100%"
              @if(count($heading)>9)
              style="width: {{count($heading)*100}}px;"
-        @endif>
+                @endif>
             @if(!empty($heading))
                 <table class="min-w-full table-fixed divide-y divide-gray-200 ">
                     <thead class="bg-gray-100">
                     <tr>
-                        <th scope="col"
-                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider">
+                        <th scope="col" style="position:sticky; left: 0; z-index: 999;"
+                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider bg-gray-100">
                             Account Head
                         </th>
 
@@ -112,14 +113,18 @@
                     </thead>
                     <tbody class="bg-white  ">
                     <tr>
-                        <th scope="col" colspan="{{ 2 + count($heading) }}"
+                        <th scope="col" style="position:sticky; left: 0; z-index: 999;"
                             class="  px-2  text-left border-r py-2  text-sm font-bold text-gray-500 ">
                             Revenue
+                        </th>
+                        <th scope="col" colspan="{{ 1 + count($heading) }}"
+                            class="  px-2  text-left border-r py-2  text-sm font-bold text-gray-500 ">
                         </th>
                     </tr>
                     @foreach(collect($report)->where('type','Income')->groupBy('account_id') as $key => $en)
                         <tr class="{{ $loop->even ? 'bg-gray-50' :'' }}">
-                            <td class="px-2   py-2   border-r text-sm   text-gray-500">
+                            <td class="px-2   py-2   border-r text-sm   text-gray-500 {{ $loop->even ? 'bg-gray-50' :'bg-white' }}"
+                                style="position:sticky; left: 0; z-index: 999;">
 
                                 {{ $en->first()['name'] }}
                             </td>
@@ -129,8 +134,9 @@
                                 @endphp
                                 <td class="px-2   py-2   text-center border-r text-sm text-gray-500">
                                     @if(!empty($first))
-                                        <a target="_blank" class="hover:text-gray-900" href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($first['date']))}}&to={{date('d M Y',strtotime($first['date']))}}">
-                                        {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat($first['balance'],2) }}
+                                        <a target="_blank" class="hover:text-gray-900"
+                                           href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($first['date']))}}&to={{date('d M Y',strtotime($first['date']))}}">
+                                            {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat($first['balance'],2) }}
                                         </a>
                                     @else
                                         -
@@ -138,15 +144,16 @@
                                 </td>
                             @endforeach
                             <td class="px-2   py-2   text-center border-r text-sm text-gray-500">
-                                <a target="_blank" class="hover:text-gray-900" href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($from_date))}}&to={{date('t M Y',strtotime($to_date))}}">
-                                {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat(collect($report)->where('account_id',$key)->sum('balance'),2) }}
+                                <a target="_blank" class="hover:text-gray-900"
+                                   href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($from_date))}}&to={{date('t M Y',strtotime($to_date))}}">
+                                    {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat(collect($report)->where('account_id',$key)->sum('balance'),2) }}
                                 </a>
                             </td>
                         </tr>
                     @endforeach
                     <tr>
-                        <th scope="col"
-                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider">
+                        <th scope="col" style="position:sticky; left: 0; z-index: 999;"
+                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider bg-white">
                             Total Revenue
                         </th>
 
@@ -170,16 +177,20 @@
                     </tr>
 
 
-                    <tr>
-                        <th scope="col" colspan="{{ 2 + count($heading) }}"
+                    <tr class="bg-white">
+                        <th scope="col" style="position:sticky; left: 0; z-index: 999;"
                             class="  px-2  text-left border-r py-2  text-sm font-bold text-gray-500 ">
                             Less Cost of Sales
+                        </th>
+                        <th scope="col" colspan="{{ 1 + count($heading) }}"
+                            class="  px-2  text-left border-r py-2  text-sm font-bold text-gray-500 ">
                         </th>
                     </tr>
 
                     @foreach(collect($report)->where('p_ref','cost-of-sales-4')->groupBy('account_id') as $key => $en)
                         <tr class="{{ $loop->even ? 'bg-gray-50' :'' }}">
-                            <td class="px-2   py-2   border-r text-sm   text-gray-500">
+                            <td class="px-2   py-2   border-r text-sm   text-gray-500 {{ $loop->even ? 'bg-gray-50' :'bg-white' }}"
+                                style="position:sticky; left: 0; z-index: 999;">
 
                                 {{ $en->first()['name'] }}
                             </td>
@@ -189,8 +200,9 @@
                                 @endphp
                                 <td class="px-2   py-2   text-center border-r text-sm text-gray-500">
                                     @if(!empty($first))
-                                        <a target="_blank" class="hover:text-gray-900" href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($first['date']))}}&to={{date('d M Y',strtotime($first['date']))}}">
-                                        {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat($first['balance'],2) }}
+                                        <a target="_blank" class="hover:text-gray-900"
+                                           href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($first['date']))}}&to={{date('d M Y',strtotime($first['date']))}}">
+                                            {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat($first['balance'],2) }}
                                         </a>
                                     @else
                                         -
@@ -198,15 +210,16 @@
                                 </td>
                             @endforeach
                             <td class="px-2   py-2   text-center border-r text-sm text-gray-500">
-                                <a target="_blank" class="hover:text-gray-900" href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($from_date))}}&to={{date('t M Y',strtotime($to_date))}}">
-                                {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat(collect($report)->where('account_id',$key)->sum('balance'),2) }}
+                                <a target="_blank" class="hover:text-gray-900"
+                                   href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($from_date))}}&to={{date('t M Y',strtotime($to_date))}}">
+                                    {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat(collect($report)->where('account_id',$key)->sum('balance'),2) }}
                                 </a>
                             </td>
                         </tr>
                     @endforeach
                     <tr>
-                        <th scope="col"
-                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider">
+                        <th scope="col" style="position:sticky; left: 0; z-index: 999;"
+                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider bg-white">
                             Total Expenses
                         </th>
 
@@ -233,7 +246,8 @@
 
                     <tr>
                         <th scope="col"
-                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider">
+                            class="  px-2 bg-white  border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider"
+                            style="position:sticky; left: 0; z-index: 999;">
                             Gross Profit
                         </th>
 
@@ -258,15 +272,21 @@
 
 
                     <tr>
-                        <th scope="col" colspan="{{ 2 + count($heading) }}"
-                            class="  px-2  text-left border-r py-2  text-sm font-bold text-gray-500 ">
+                        <th scope="col"
+                            style="position:sticky; left: 0; z-index: 999;"
+                            class="  px-2  text-left border-r py-2  text-sm font-bold text-gray-500 bg-white ">
                             Other Expenses
                         </th>
+                        <th scope="col" colspan="{{ 1 + count($heading) }}"
+                            class="  px-2  text-left border-r py-2  text-sm font-bold text-gray-500 bg-white ">
+                        </th>
+
                     </tr>
 
                     @foreach(collect($report)->where('type','Expenses')->where('p_ref','!=','cost-of-sales-4')->groupBy('account_id') as $key => $en)
                         <tr class="{{ $loop->even ? 'bg-gray-50' :'' }}">
-                            <td class="px-2   py-2   border-r text-sm   text-gray-500">
+                            <td class="px-2   py-2   border-r text-sm   text-gray-500 {{ $loop->even ? 'bg-gray-50' :'bg-white' }} "
+                                style="position:sticky; left: 0; z-index: 999;">
 
                                 {{ $en->first()['name'] }}
                             </td>
@@ -276,8 +296,9 @@
                                 @endphp
                                 <td class="px-2   py-2   text-center border-r text-sm text-gray-500">
                                     @if(!empty($first))
-                                        <a target="_blank" class="hover:text-gray-900" href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($first['date']))}}&to={{date('d M Y',strtotime($first['date']))}}">
-                                        {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat($first['balance'],2) }}
+                                        <a target="_blank" class="hover:text-gray-900"
+                                           href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($first['date']))}}&to={{date('d M Y',strtotime($first['date']))}}">
+                                            {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat($first['balance'],2) }}
                                         </a>
                                     @else
                                         -
@@ -285,15 +306,16 @@
                                 </td>
                             @endforeach
                             <td class="px-2   py-2   text-center border-r text-sm text-gray-500">
-                                <a target="_blank" class="hover:text-gray-900" href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($from_date))}}&to={{date('t M Y',strtotime($to_date))}}">
-                                {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat(collect($report)->where('account_id',$key)->sum('balance'),2) }}
+                                <a target="_blank" class="hover:text-gray-900"
+                                   href="{{ url('accounts/accountant/ledger') }}?account_id={{$key}}&from={{date('d M Y',strtotime($from_date))}}&to={{date('t M Y',strtotime($to_date))}}">
+                                    {{ \Devzone\Ams\Helper\GeneralJournal::numberFormat(collect($report)->where('account_id',$key)->sum('balance'),2) }}
                                 </a>
                             </td>
                         </tr>
                     @endforeach
                     <tr>
-                        <th scope="col"
-                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider">
+                        <th scope="col" style="position:sticky; left: 0; z-index: 999;"
+                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider bg-white">
                             Total Other Expenses
                         </th>
 
@@ -318,8 +340,8 @@
                     </tr>
 
                     <tr>
-                        <th scope="col"
-                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider">
+                        <th scope="col" style="position:sticky; left: 0; z-index: 999;"
+                            class="  px-2    border-r py-2 text-left text-sm font-bold text-gray-500  tracking-wider bg-white">
                             Net Profit/(Loss)
                         </th>
 
