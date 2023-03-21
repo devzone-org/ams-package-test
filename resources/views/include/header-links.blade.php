6e1c50f1123e898a1fdc2ffc51c6fc1900828c7f
Arslan Ejaz <arslan.ejaz@devzone.pk>
2023-03-21 16:39:24 +0500

    Equity Ratio 21th March

--- resources/views/include/header-links.blade.php
+++ resources/views/include/header-links.blade.php
@@ -188,6 +188,24 @@
                         </div>
                     </a>
 
+                    <a href="{{ url('accounts/accountant/equity-ratio') }}"
+                       class="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-100 {{   (Request::segment(3) == 'equity-ratio')  ? 'bg-gray-100' : ''}} ">
+                        <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none" stroke="currentColor"
+                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
+                        </svg>
+
+                        <div class="ml-4">
+                            <p class="text-base font-medium text-gray-900">
+                                Equity Ratio
+                            </p>
+                            <p class="mt-1 text-sm text-gray-500">
+                                Equity ratio of accounts.
+                            </p>
+                        </div>
+                    </a>
+
                 </div>
 
             </div>
---
+++ src/Http/Livewire/ChartOfAccount/EquityRatio.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Devzone\Ams\Http\Livewire\ChartOfAccount;
+
+use Livewire\Component;
+
+class EquityRatio extends Component
+{
+    public $equity_data;
+
+    public function mount()
+    {
+        $this->equityData();
+    }
+
+    public function equityData()
+    {
+        try {
+            $this->equity_data = \Devzone\Ams\Models\EquityRatio::from('equity_ratio as er')
+                ->join('chart_of_accounts as coa1', 'coa1.id', 'er.account_id')
+                ->join('chart_of_accounts as coa2', 'coa2.id', 'er.drawing_account_id')
+                ->select('er.partner_name', 'er.ratio', 'coa1.name as account_name', 'coa2.name as drawing_account_name')
+                ->get()
+                ->toArray();
+        } catch (\Exception $e) {
+            $this->addError('error', $e->getMessage());
+        }
+
+    }
+    public function render()
+    {
+        return view('ams::livewire.chart-of-accounts.equity-ratio');
+    }
+}
