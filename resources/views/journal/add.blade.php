5f041a4d3517d728eddb60ea4b8fe8443457f542
Bilal Ejaz <bilal.ejaz@devzone.pk>
2023-04-11 16:42:34 +0500

    here

--- resources/views/journal/add.blade.php
+++ resources/views/journal/add.blade.php
@@ -3,8 +3,13 @@
 @section('title') Add Journal Entry @endsection
 
 @section('content')
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-
+    @if(env('AMS_BOOTSTRAP') == 'true')
         @livewire('journal.add')
-    </div>
+
+    @else
+        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+
+            @livewire('journal.add')
+        </div>
+    @endif
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,4 +1,6 @@
-@if(env('ACCOUNTS_LAYOUT','topnav') == 'topnav')
+@if(env('AMS_BOOTSTRAP') == 'true')
+    @include('layouts.master')
+@elseif(env('ACCOUNTS_LAYOUT','topnav') == 'topnav')
     @include('ams::include.layouts.topnav')
 @elseif(env('ACCOUNTS_LAYOUT') == 'sidebar')
     @include('ams::include.layouts.sidebar')
