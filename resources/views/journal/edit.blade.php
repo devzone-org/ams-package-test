d2b0c59700d55ae6ce1471cedc7492b1703ba5a9
Bilal Ejaz <bilal.ejaz@devzone.pk>
2023-04-18 13:33:36 +0500

    here

--- resources/views/journal/edit.blade.php
+++ resources/views/journal/edit.blade.php
@@ -6,8 +6,7 @@
 
 @section('content')
     @if(env('AMS_BOOTSTRAP') == 'true')
-{{--        @livewire('journal.edit',['voucher_no'=> $voucher_no])--}}
-        abc def asdasdasjdaskldasjkldhasjdhasjdhsajkdhasjkdhasjkdhasjkdh
+        @livewire('journal.edit',['voucher_no'=> $voucher_no])
     @else
         <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             @livewire('journal.edit',['voucher_no'=> $voucher_no])
