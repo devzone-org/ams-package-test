20caedb897fd29e71e52513f90bfd9b3c4acf5ae
Arslan Ejaz <arslan.ejaz@devzone.pk>
2024-01-09 15:28:32 +0500

    here

--- resources/views/closing/closing-fiscal-year.blade.php
+++ resources/views/closing/closing-fiscal-year.blade.php
@@ -1,13 +1,14 @@
 @extends('ams::layouts.master')
 
-@section('title') Closing Fiscal Year @endsection
+@section('title')
+    Closing Fiscal Year
+@endsection
 
 @section('content')
-    <div class=" mx-auto py-6 sm:px-6 lg:px-8">
-
-
-        @livewire('closing.closing-fiscal-year')
-
-
-    </div>
+    @if (env('AMS_BOOTSTRAP') == 'true')
+    @else
+        <div class=" mx-auto py-6 sm:px-6 lg:px-8">
+            @livewire('closing.closing-fiscal-year')
+        </div>
+    @endif
 @endsection
