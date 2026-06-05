7da3472be6d7c6a3b176a6b0a988f18efdf9e80f
dz-touqeer-asad <touqeer.asad@devzone.pk>
2026-06-05 19:08:11 +0500

    wip

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -67,6 +67,38 @@
                 </button>
             </div>
             <div class="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center">
+
+                @if (file_exists(public_path('css/dark-mode.css')))
+                    <div class="ml-3 flex items-center space-x-3 mr-8">
+                        <button
+                                type="button"
+                                class="theme-toggle-switch"
+                                title="Toggle dark mode"
+                                tabindex="0"
+                                onclick="
+                            const isDark = document.documentElement.classList.toggle('dark');
+                            document.documentElement.classList.toggle('dark-ams', isDark);
+                            localStorage.setItem('theme', isDark ? 'dark' : 'light');
+                          "
+                        >
+                            <div class="theme-toggle-knob">
+                                <!-- Sun icon for light mode -->
+                                <svg class="theme-toggle-icon theme-sun-icon" fill="currentColor" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                          d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                          clip-rule="evenodd"/>
+                                </svg>
+
+                                <!-- Moon icon for dark mode -->
+                                <svg class="theme-toggle-icon theme-moon-icon" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"/>
+                                </svg>
+                            </div>
+                        </button>
+                    </div>
+                @endif
+
+
                 <p class="rounded-md py-2 px-3 text-sm inline-flex text-right font-medium text-gray-900   hover:text-gray-900">
                     {{ Auth::user()->name }}<br>{{date('d F Y h:i A')}}</p>
                 <div class="flex-shrink-0 relative">
